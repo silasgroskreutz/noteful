bf647a128131d17849d9c763ccc1bc7539b5d973
Silas Groskreutz <50895337+silasgroskreutz@users.noreply.github.com>
2019-11-22 14:22:45 -0600

    updated config to point to herkou

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: 'https://dry-journey-88257.herokuapp.com/api',
+    API_ENDPOINT: 'https://vast-basin-01840.herokuapp.com/api',
     API_KEY: process.env.REACT_APP_API_KEY
   }
