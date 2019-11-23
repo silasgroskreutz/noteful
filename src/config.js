75c4e3aee507c3f0ed55cca8f6d05a5fba3fcc58
Silas Groskreutz <50895337+silasgroskreutz@users.noreply.github.com>
2019-11-23 11:20:24 -0600

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,3 @@
 export default {
-  API_ENDPOINT: `https://desolate-meadow-95722.herokuapp.com/`,
-  API_KEY: process.env.REACT_APP_API_KEY,
+  API_ENDPOINT: `https://desolate-meadow-95722.herokuapp.com/`
 }
