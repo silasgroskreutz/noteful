31e04eb4eaec21fd10f4e872d24d08fa6b729f95
Silas Groskreutz <50895337+silasgroskreutz@users.noreply.github.com>
2019-11-23 10:53:42 -0600

    updated connection to heroku

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: `https://vast-basin-01840.herokuapp.com`,
+    API_ENDPOINT: `https://aqueous-plateau-94775.herokuapp.com`,
     API_KEY: process.env.REACT_APP_API_KEY
   }
